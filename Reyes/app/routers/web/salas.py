4ffafdc7910332058d4bbf1414442fb5ed7c3928
delestal <delestalmr@gmail.com>
2025-11-27 12:02:19 +0100

    lista de salas

--- Reyes/app/routers/web/salas.py
+++ Reyes/app/routers/web/salas.py
@@ -1,5 +1,5 @@
-from fastapi.templating import Jinja2Templates
-from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates 
+from fastapi import APIRouter, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -19,4 +19,18 @@ def list_salas(request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse(
         "salas/list.html",
         {"request": request, "salas": salas}
+    )
+    
+# detalle de sala (http://localhost:8000/salas/3)
+@router.get("/{sala_id}", response_class=HTMLResponse)
+def salas_detail(request: Request, sala_id: int, db: Session = Depends(get_db)):
+    sala= db.execute(select(SalaORM).where(SalaORM.id == sala_id)).scalar_one_or_none()
+    
+    if sala is None:
+        raise HTTPException(status_code=404, detail="404 - Sala no encontrada")
+
+    
+    return templates.TemplateResponse(
+        "salas/detail.html",
+        {"request": request, "sala": sala}
     )
