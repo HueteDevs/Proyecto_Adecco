d0f529baf7f392c49e3d8cd92e25b912888c9e2a
Kary_H <kityharo@hotmail.com>
2025-12-01 12:12:20 +0100

    init_router_web

--- app/routers/web/__init__.py
+++ app/routers/web/__init__.py
@@ -2,10 +2,19 @@
 Router de páginas web
 Contienen los endpoints que renderizan HTMLs
 """
-from app.routers.web import horarios
+from app.routers.web import pelicula
+from app.routers.web import sala
+from app.routers.web import venta
+from app.routers.web import genre
+from app.routers.web import horario
 
 from fastapi import APIRouter
 
-
 router = APIRouter()
+
+router.include_router(pelicula.router)
+router.include_router(sala.router)
+router.include_router(venta.router)
+router.include_router(genre.router)
+router.include_router(horario.router)
 
