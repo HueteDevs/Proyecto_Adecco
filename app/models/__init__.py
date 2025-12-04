6993bebdea6a0ab34e599c3d7a30df16cf96c2ab
Kary_H <kityharo@hotmail.com>
2025-12-04 13:28:06 +0100

    models_init_completado

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,10 +2,12 @@
 Modelos de base de datos (SQLAlchemy)
 """
 
+# Todos los modelos han sido incluídos (Kary)
+
 from app.models.horario import Horario
 from app.models.genre import Genre
 from app.models.pelicula import Pelicula
-"from app.models.sala import Sala"
-"from app.models.venta import Venta"
+from app.models.sala import Sala
+from app.models.venta import Venta
 
-__all__ = [Horario]
+__all__ = ["Pelicula", "Sala", "Horario", "Genre", "Venta"]
