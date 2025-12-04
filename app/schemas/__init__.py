a44011b3963f49faf4f425bf851adb8a86ff9d20
Manitusitu <paquitopan101@outlook.com>
2025-12-04 10:15:21 +0100

    relaciones de horario hacia salas y peliculas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -3,9 +3,6 @@ Esquemas Pydantic para validación de datos
 """
 
 from app.schemas.horario import HorarioResponse, HorarioCreate, HorarioUpdate, HorarioPatch
-
-__all__ = ["HorarioResponse", "HorarioCreate", "HorarioUpdate", "HorarioPatch"]
-
 from app.schemas.sala import SalaResponse, SalaCreate, SalaUpdate   
 
-__all__ = ["SalaResponse", "SalaCreate", "SalaUpdate"]  
+__all__ = ["HorarioResponse", "HorarioCreate", "HorarioUpdate", "HorarioPatch","SalaResponse", "SalaCreate", "SalaUpdate"]  
